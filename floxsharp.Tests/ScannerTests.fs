56e9819d4e5305850f9bfb87321962d9ff989ded
Vaas <streltcov.vasiliy.bpo1401@yandex.ru>
2021-02-23 18:01:58 +0700

    Added more tests

--- floxsharp.Tests/ScannerTests.fs
+++ floxsharp.Tests/ScannerTests.fs
@@ -19,21 +19,21 @@ type ScannerTests () =
         let source = "({*-+.;},"
         let actual = getTokenTypes source
         let expected = [TokenType.LeftParen; TokenType.LeftBrace; TokenType.Star; TokenType.Minus; TokenType.Plus; TokenType.Dot; TokenType.Semicolon; TokenType.RightBrace; TokenType.Comma]
-        Assert.AreEqual(actual, expected)
+        Assert.AreEqual(expected, actual)
 
     [<TestMethod>]
     member this.ScanDoubleTokens () =
         let source = ">= <= !=       =="
         let actual = getTokenTypes source
         let expected = [TokenType.GreaterEqual; TokenType.LessEqual; TokenType.BangEqual; TokenType.EqualEqual]
-        Assert.AreEqual(actual, expected)
+        Assert.AreEqual(expected, actual)
 
     [<TestMethod>]
     member this.ScanSingleAndDoubleTokens () =
         let source = "=== =<="
         let actual = getTokenTypes source
         let expected = [TokenType.EqualEqual; TokenType.Equal; TokenType.Equal; TokenType.LessEqual]
-        Assert.AreEqual(actual, expected)
+        Assert.AreEqual(expected, actual)
 
     [<TestMethod>]
     member this.ScanSingleComment () = 
@@ -46,14 +46,14 @@ type ScannerTests () =
         let source = "\"asdf\"\"qwer\""
         let actual = getTokenTypesAndLexemes source
         let expected = [(TokenType.String, "asdf"); (TokenType.String, "qwer")]
-        Assert.AreEqual(actual, expected)
+        Assert.AreEqual(expected, actual)
 
     [<TestMethod>]
     member this.ScanKeywords () =
         let source = "var while nil    or and for//true"
         let actual = getTokenTypes source
         let expected = [TokenType.Var; TokenType.While; TokenType.Nil; TokenType.Or; TokenType.And; TokenType.For; TokenType.Comment]
-        Assert.AreEqual(actual, expected)
+        Assert.AreEqual(expected, actual)
 
     [<TestMethod>]
     member this.ScanExpression () = 
@@ -62,10 +62,24 @@ type ScannerTests () =
         let expected = [(TokenType.Var, "var"); (TokenType.Identifier, "nullValue"); (TokenType.Equal, ""); 
             (TokenType.Nil, "nil"); (TokenType.Semicolon, ""); (TokenType.If, "if"); (TokenType.Identifier, "nullValue");
             (TokenType.EqualEqual, ""); (TokenType.Nil, "nil"); (TokenType.Print, "print"); (TokenType.Semicolon, "")]
-        Assert.AreEqual(actual, expected)
+        Assert.AreEqual(expected, actual)
+
+    [<TestMethod>]
+    member this.ScanExpressionWithComment () =
+        let source = "var str = \"abc\"; \n // this is a comment"
+        let actual = getTokenTypesAndLexemes source
+        let expected = [(TokenType.Var, "var"); (TokenType.Identifier, "str"); (TokenType.Equal, ""); 
+            (TokenType.String, "abc"); (TokenType.Semicolon, ""); (TokenType.Comment, "")]
+        Assert.AreEqual(expected, actual)
 
     [<TestMethod>]
     [<ExpectedException(typeof<Common.InterpreterException>)>]
     member this.ScanUnexpectedSymbol () = 
         let source = "var a = &&&"
+        getTokenTypes source |> ignore
+
+    [<TestMethod>]
+    [<ExpectedException(typeof<Common.InterpreterException>)>]
+    member this.ScanUnterminatedString () =
+        let source = "var string = \"asdf ;"
         getTokenTypes source |> ignore
