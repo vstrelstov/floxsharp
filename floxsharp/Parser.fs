b9a917e824a1d0db4aef55709cac151ab3620e5b
vstreltsov <streltcov.vasiliy.bpo1401@yandex.ru>
2021-04-23 22:13:48 +0300

    Added more stubs for parser functions

--- floxsharp/Parser.fs
+++ floxsharp/Parser.fs
@@ -28,17 +28,26 @@ module Parser =
 
         printExpression expression
     
-    let private matchToken list =
-        let matchType tokenTypes = 
+    let private matchToken tokenTypes =
+        let matchType list = 
             let head = List.tryHead list
             Option.isSome head && Array.contains head.Value.Type tokenTypes
         matchType
+
+    let private parseComparison () = () // Stub to be implemented later
+    
+    let private parseEquality tokensList = 
+        let equalityTokenTypes = [|TokenType.BangEqual; TokenType.EqualEqual|]
+        
+        Binary (Literal (null), { Type = TokenType.EOF; Lexeme = ""; Line = 0}, Literal (null))// TODO: Replace stub values
+    
+    let private parseExpression () = parseEquality List.empty<Token> // Stub to be implemented later
 
     let private primary list = // Stub function
         Literal null
     
     let rec private unary list = 
-        if matchToken list [|TokenType.Bang; TokenType.Minus|] then
+        if matchToken [|TokenType.Bang; TokenType.Minus|] list then
             let operator = List.tryHead list // TODO: Probably should raise an exception if list is empty
             let right = unary (Common.tryTail list)
             Unary (operator.Value, right)
